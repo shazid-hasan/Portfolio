985148b4c5e0bdd6aa38d6eec5f83dc24f386cb8
Shazid Hasan <amj.shazidhasan@gmail.com>
2023-12-29 01:43:12 +0600

    second commit

--- project/firstapp/views.py
+++ project/firstapp/views.py
@@ -4,10 +4,10 @@ def home(request):
     return render(request,'index1.html')
 
 def inner(request):
-    return render(request,'inner-page.html')
+    return render(request,'inner-page1.html')
 
 def port(request):
-    return render(request,'portfolio-details.html')
+    return render(request,'portfolio-details1.html')
 
 
 # Create your views here.
